763edb15213961904fcf5d45badcfa344c7e5205
redrover9 <grace.thompson97@gmail.com>
2020-11-27 11:49:36 -0800

    Update canada_post_tracker.py

--- canada_post_tracker.py
+++ canada_post_tracker.py
@@ -8,47 +8,53 @@ import tkinter as tk
 from tkinter import Frame
 from tkinter import messagebox
 class App(Frame):
-    def __init__(self, parent=None):
-        Frame.__init__(self, parent)
-        self.parent = parent
-        self.pack()
-        self.make_widgets()
-        super().__init__()
-    def make_widgets(self):
-        self.winfo_toplevel().title("Package Tracker")
+	def __init__(self, parent=None):
+		Frame.__init__(self, parent)
+		self.parent = parent
+		self.pack()
+		self.make_widgets()
+		super().__init__()
+	def make_widgets(self):
+		self.winfo_toplevel().title("Package Tracker")
 
-        tk.Label(self, text="Enter a tracking number to check whether a package has been delivered: ", name="myLabel").grid(row=0, column=0)
-        self.entry = tk.Entry(self)
-        self.entry.grid(row=0, column=1)
+		tk.Label(self, text="Enter a tracking number to check whether a package has been delivered: ", name="myLabel").grid(row=0, column=0)
+		self.entry = tk.Entry(self)
+		self.entry.grid(row=0, column=1)
 
-        btn = tk.Button(self, text="Submit", command=self.on_submit)
-        btn.grid(row=2, column=0, columnspan=2, sticky="ew")
+		btn = tk.Button(self, text="Submit", command=self.on_submit)
+		btn.grid(row=2, column=0, columnspan=2, sticky="ew")
 
-    def on_submit(self):
-        inputNum = self.entry.get()
-        os.environ['MOZ_HEADLESS'] = '1'
-        driver = webdriver.Firefox("/usr/local/bin/")
-        driver.get("https://www.canadapost.ca/cpc/en/home.page")
-        assert "Canada Post" in driver.title
-        trackingNumber = driver.find_element_by_name("trackingNumber")
-        trackingNumber.clear()
-        trackingNumber.send_keys(inputNum)
-        #7060220906288741
-        #7060220551634771
-        trackingNumber.send_keys(Keys.RETURN)
-        if len(inputNum) > 16:
-            csvDownload = driver.find_element_by_id("downloadCSVResults")
-            driver.close()
-        try:
-            element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "track_package_result_details")))
-            assert "Notice card" in driver.page_source
-            tk.messagebox.showinfo("Attention", "The package has been delivered.")
-        except AssertionError:
-            tk.messagebox.showerror("Attention", "The package has not yet been delivered.")
-        except TimeoutException:
-            tk.messagebox.showerror("Attention", "The package has not yet been delivered.")
-        driver.close()
+	def on_submit(self):
+		inputNum = self.entry.get()
+		os.environ['MOZ_HEADLESS'] = '1'
+		profile = webdriver.FirefoxProfile()
+		profile.set_preference('browser.download.folderList', 1)
+		profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
+		driver = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver", firefox_profile=profile)
+		driver.get("https://www.canadapost.ca/cpc/en/home.page")
+		
+		assert "Canada Post" in driver.title
+		trackingNumber = driver.find_element_by_name("trackingNumber")
+		trackingNumber.clear()
+		trackingNumber.send_keys(inputNum)
+		#7060220906288741
+		#7060220551634771
+		trackingNumber.send_keys(Keys.RETURN)
+		if len(inputNum) > 16:
+			csvElement = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID,"downloadCSVResults")))
+			csvElement.click()
+			assert "Download Track results" in driver.page_source
+			tk.messagebox.showinfo("Attention", "The package statuses have been saved in the Downloads folder.")
+			driver.close()
+		try:
+			element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "track_package_result_details")))
+			assert "Notice card" in driver.page_source
+			tk.messagebox.showinfo("Attention", "The package has been delivered.")
+		except AssertionError:
+			tk.messagebox.showerror("Attention", "The package has not yet been delivered.")
+		driver.close()
 
 
 if __name__ == '__main__':
-    App().mainloop()
+	App().mainloop()
+		
